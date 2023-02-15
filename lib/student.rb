14e090d19b79fcd95b5927a128e6d482e27c413a
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-15 11:08:53 -0500

    create student class, define initialize method

--- lib/student.rb
+++ lib/student.rb
@@ -1,1 +1,11 @@
-
+class Student
+  attr_reader :name,
+              :age,
+              :scores
+              
+  def initialize(hash)
+    @name = hash[:name]
+    @age = hash[:age]
+    @scores = []
+  end
+end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -3,12 +3,13 @@ require './lib/student'
 
 RSpec.describe Student do
   describe '#initialize' do
-    it 'exists'
-    student = Student.new({:name = 'Morgan', :age = 21})
+    it 'exists' do
+    student = Student.new({:name => 'Morgan', :age => 21})
 
     expect(student).to be_a(Student)
     expect(student.name).to eq('Morgan')
     expect(student.age).to eq(21)
     expect(student.scores).to eq([])
+    end
   end
 end

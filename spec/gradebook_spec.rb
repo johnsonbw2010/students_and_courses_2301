1ccbf9956694b0009c3895557bf0d4fca8182a63
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-15 12:36:40 -0500

    add students_below tests

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -41,4 +41,28 @@ RSpec.describe Gradebook do
       expect(gradebook.list_all_students['Economics']).to eq([student2])
     end
   end
+
+  describe '#students_below' do
+    it 'can return students below a grade threshold' do
+      gradebook = Gradebook.new('Mr. White')
+      course1 = Course.new('Geometry', 2)
+      course2 = Course.new('Economics', 3)
+      student1 = Student.new({name: 'Morgan', age: 21})
+      student2 = Student.new({name: 'Jordan', age: 29})
+      course1.enroll(student1)
+      course2.enroll(student2)
+      gradebook.add_course(course1)
+      gradebook.add_course(course2)
+      student1.log_score(90)
+      student1.log_score(85)
+      student1.log_score(88)
+      student2.log_score(85)
+      student2.log_score(86)
+      student2.log_score(82)
+
+      expect(students_below(87)).to eq([student2])
+      expect(students_below(84)).to eq([])
+      expect(students_below(88)).to eq([student1, student2])
+    end
+  end
 end

bb4be5bed55404bc89902507eb5e4913ce94c029
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-15 12:24:07 -0500

    add list_all_students tests

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -21,9 +21,25 @@ RSpec.describe Gradebook do
       expect(gradebook.courses.size).to eq(1)
       
       gradebook.add_course(course2)
-      
       expect(gradebook.courses.size).to eq(2)
       expect(gradebook.courses).to all(be_a Course)
+    end
+  end
+
+  describe '#list_all_students' do
+    it 'can list all students in all courses' do
+      gradebook = Gradebook.new('Mr. White')
+      course1 = Course.new('Geometry', 2)
+      course2 = Course.new('Economics', 3)
+      student1 = Student.new({name: 'Morgan', age: 21})
+      student2 = Student.new({name: 'Jordan', age: 29})
+      course1.enroll(student1)
+      course2.enroll(student2)
+      gradebook.add_course(course1)
+      gradebook.add_course(course2)
+      
+      expect(gradebook.list_all_students['Geometry']).to eq([student1])
+      expect(gradebook.list_all_students['Economics']).to eq([student2])
     end
   end
 end

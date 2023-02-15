1d8e95938b65c71281046a84cdb3e39c00565d21
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-15 11:53:51 -0500

    add Gradebook class

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,1 +1,9 @@
+class Gradebook
+  attr_reader :instructor,
+              :courses
 
+  def initialize(instructor)
+    @instructor = instructor
+    @courses = []
+  end
+end
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -3,9 +3,11 @@ require 'spec_helper'
 RSpec.describe Gradebook do
   describe '#initialize' do
     it 'exists' do
-      gradebook = Gradebook.new(instructor, courses)
+      gradebook = Gradebook.new('Mr. White')
 
       expect(gradebook).to be_a(Gradebook)
+      expect(gradebook.instructor).to eq('Mr. White')
+      expect(gradebook.courses).to eq([])
     end
   end
 end

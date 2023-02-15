bca1c263b6e8473c31028c932dbf1750c298c2cd
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-15 11:31:53 -0500

    add full? test

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Course do
       expect(course.students).to eq([])
     end
   end
+
+  describe '#full?' do
+    it 'is not full when initialized' do
+      course = Course.new('Calculus', 2)
+
+      expect(course.full?).to eq(false)
+    end
+  end
 end

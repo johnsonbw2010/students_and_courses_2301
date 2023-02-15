d6e8feff625cee911e509151d64d9065f7041f42
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-15 11:58:43 -0500

    add add_course tests

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe Gradebook do
       expect(gradebook.courses).to eq([])
     end
   end
+
+  describe '#add_course' do
+    it 'can contain courses' do
+      gradebook = Gradebook.new('Mr. White')
+      course1 = Course.new('Geometry', 2)
+      course2 = Course.new('Economics', 3)
+      gradebook.add_course(course1)
+      
+      expect(gradebook.courses.size).to eq(1)
+      
+      gradebook.add_course(course2)
+      
+      expect(gradebook.courses.size).to eq(2)
+      expect(gradebook.courses).to all(be_a Course)
+    end
+  end
 end
